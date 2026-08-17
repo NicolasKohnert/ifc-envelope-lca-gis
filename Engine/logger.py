945bbb47a05790a9500d6548632ffaa265e38377
NicolasKohnert <nicolas.kohnert@t-online.de>
2026-08-17 05:15:16 +0200

    Setup logger.py

---
+++ Engine/logger.py
@@ -0,0 +1,24 @@
+import logging
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent.parent
+
+def setup_logger():
+    log_dir = BASE_PATH / "logs"
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    log_file = log_dir / "app.log"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger("BIM-GIS_Pipeline")
+
+if __name__ == "__main__":
+    logger = setup_logger()
+    logging.info("Test-Log: Successfully set up the logger.")
